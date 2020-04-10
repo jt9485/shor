320fa0219f3543e0597e74c4bac97c6d524bb3fe
linguinichico <63466261+linguinichico@users.noreply.github.com>
2020-04-10 23:35:11 +0100

    Update shor.py
    FAZ SENTIDO A CONDIÇÃO? factor é sempre um divisor de n por isso a condicao que se pos vai ser sempre falsa, certo?
    Com isto dito, não basta achar um dos fatores não triviais para o output desta função?
    
    Defini o sz1 como sendo o número de t qubits e o sz2 como o número de n qubits
    O t já está definido de uma forma geral, tirei a informação da página 16 antes da seccção 7

--- shor.py
+++ shor.py
@@ -18,13 +18,13 @@ def shor(n):
         if r % 2 != 1 and a**(r/2) % n != n-1:
         	found = True
         	factor = math.gdc(a**(r/2)+1, n)
-        	if n % factor != 0:
-            	factor = math.gcd(a**(r/2)-1, n)
 
     return factor
 
 #TODO
 def shor_quantum(n):
-    sz = math.ceil(math.log2(n))
+    sz1 = random.randint(math.ceil(math.log2(n**2)), math.floor(2 * math.log2(n**2)))
+    sz2 = math.ceil(math.log2(n))
+    sz = sz1 + sz2
     register = np.empty(sz, dtype='O')
     pass
