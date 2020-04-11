60dd2ce7047e987da3fdee68abe46b850cf5c73f
João Tavares <jcostavares@gmail.com>
2020-04-11 15:50:53 +0100

    Implemented powermod + Started implementing quantum_shor

--- shor.py
+++ shor.py
@@ -2,6 +2,17 @@ import math
 import random
 import numpy as np
 
+def power_mod(a, exp, m):
+    c = 1
+    while exp > 0:
+        if np.mod(exp, 2):
+            c = np.mod(c*a, m)
+
+        a = np.mod(a*a, m)
+        exp = np.floor_divide(exp, 2)
+
+    return c
+
 def shor(n):
     found = False
 
@@ -13,7 +24,9 @@ def shor(n):
         	found = True
         	factor = gcd
 
-        r = shor_quantum(n, a)
+        measure = shor_quantum(n, a)
+
+        #TODO extract the denominator
 
         if r % 2 != 1 and a**(r/2) % n != n-1:
         	found = True
@@ -22,9 +35,40 @@ def shor(n):
     return factor
 
 #TODO
-def shor_quantum(n):
-    sz1 = random.randint(math.ceil(math.log2(n**2)), math.floor(2 * math.log2(n**2)))
-    sz2 = math.ceil(math.log2(n))
-    sz = sz1 + sz2
-    register = np.empty(sz, dtype='O')
-    pass
+def shor_quantum(N, x, t):
+    n = math.ceil(math.log2(N))
+
+    # phi_0 = |0...0>
+    reg_sz = t+n
+    reg_num_states = 1
+
+    # phi_1 = \frac{1}{\sqrt{2^t}} \sum_{j=0}^{2^t-1}|j>|0>
+    reg_num_states = 2 ** t
+
+    # phi_2 = \frac{1}{\sqrt{2^t}} \sum_{j=0}^{2^t-1}|j>|x^j \Mod{N}>
+
+    # phi_3 after measuring second register
+
+    # Measure
+    j = random.randint(0, 2**t-1)
+    x_b = powermod(x, j, N)
+    prob = np.empty(reg_num_states, dtype=np.double)
+
+    # probability not equal anymore
+    reg_num_states = 0
+    for j in range(2**t):
+        if x_b == powermod(x, j, N):
+            reg_num_states += 1
+
+    for j in range(2**t):
+        if x_b == powermod(x, j, N):
+            prob[j] = 1 / reg_num_states
+        else:
+            prob[j] = 0
+
+    # phi_4 apply reverse DFT
+
+    # phi_5 after measuring all qubits
+    measure = numpy.random.choice(range(2**t), p=prob)
+
+    return measure
